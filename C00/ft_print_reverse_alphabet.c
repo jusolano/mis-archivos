c0a113ce4829374087b469210bd47e749512899f
jusolano <jusolano@student.42lisboa.com>
2025-08-01 13:19:01 +0100

    Add files via upload

---
+++ C00/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusolano <jusolano@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 10:49:20 by jusolano          #+#    #+#             */
+/*   Updated: 2025/08/01 10:50:19 by jusolano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
