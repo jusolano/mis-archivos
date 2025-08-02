c2d4ca2ce861d45bacc2e874ecf77a75f141b644
jusolano <jusolano@student.42lisboa.com>
2025-08-02 11:29:54 +0100

    Add files via upload

---
+++ C02/ex00/ft_strcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusolano <jusolano@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 10:38:59 by jusolano          #+#    #+#             */
+/*   Updated: 2025/08/02 10:43:28 by jusolano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+int main(){
+	char *dest="hola";
+	char *src="oooo";
+	ft_strcpy(dest,src);
+	printf("%s", dest);
+}
