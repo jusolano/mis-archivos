63a00d893d229548c03c25f9c03d7dd88995829e
jusolano <jusolano@student.42lisboa.com>
2025-08-13 07:18:45 +0100

    Add files via upload

---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusolano <jusolano@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 08:40:18 by jusolano          #+#    #+#             */
+/*   Updated: 2025/08/12 09:57:55 by jusolano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+//#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str)
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+int	ft_total_len(int size, char **strs, char *sep)
+{
+	int	i;
+	int	total_lenght;
+
+	i = 0;
+	total_lenght = 0;
+	while (i < size)
+	{
+		total_lenght += ft_strlen(strs[i]);
+		i++;
+	}
+	total_lenght += ft_strlen(sep) * (size - 1);
+	return (total_lenght);
+}
+
+void	ft_cat(char *str_final, int size, char **strs, char *sep)
+{
+	int	k;
+	int	j;
+	int	i;
+	int	lenght;
+
+	lenght = ft_total_len(size, strs, sep);
+	i = 0;
+	k = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j] != '\0')
+			str_final[k++] = strs[i][j++];
+		if (k < lenght)
+		{
+			j = 0;
+			while (sep[j] != '\0')
+				str_final[k++] = sep[j++];
+		}
+		i++;
+	}
+	str_final[k] = '\0';
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		lenght;
+	char	*str_final;
+
+	lenght = ft_total_len(size, strs, sep);
+	str_final = (char *)malloc(sizeof(char) * (lenght + 1));
+	if (str_final == NULL)
+		return (NULL);
+	ft_cat(str_final, size, strs, sep);
+	return (str_final);
+}
+
+/*int main()
+{
+	char *fruits[] = {"apple", "banana", "pineapple"};
+	char *joined = ft_strjoin(3, fruits, "::;::");
+	printf("Resultado: %s\n", joined);
+	free(joined); 
+	return (0);
+}*/
