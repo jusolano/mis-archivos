a9f84669cdd8154cf7fb6c7240ec1e59bc87cdba
jusolano <jusolano@student.42lisboa.com>
2025-08-13 15:14:15 +0100

    Add files via upload

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusolano <jusolano@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/13 14:08:26 by jusolano          #+#    #+#             */
+/*   Updated: 2025/08/13 14:54:26 by jusolano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	char	c;
+
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+	}
+	c = '0' + (nb % 10);
+	write(1, &c, 1);
+	write(1, "\n", 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str != NULL)
+	{
+		ft_putstr(par[i].str);
+		ft_putnbr(par[i].size);
+		ft_putstr(par[i].copy);
+		i++;
+	}
+}
