cc36a696841efeaf1f3b704e606e38da791c3244
Juan Solano <juansolano@macbookair.lan>
2025-08-14 12:14:53 +0100

    finally updated

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jusolano <jusolano@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: juansolano <juansolano@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 09:52:32 by jusolano          #+#    #+#             */
-/*   Updated: 2025/08/11 10:03:14 by jusolano         ###   ########.fr       */
+/*   Updated: 2025/08/14 12:13:05 by juansolano       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-//#include <stdio.h>
+#include <stdio.h>
 
 int	*ft_range(int min, int max)
 {
@@ -20,11 +20,11 @@ int	*ft_range(int min, int max)
 
 	if (min >= max)
 		return (NULL);
-	range = (int *)malloc(sizeof(int) * (max - min + 1));
+	range = (int *)malloc(sizeof(int) * (max - min));
 	if (range == NULL)
 		return (NULL);
 	i = 0;
-	while (min <= max)
+	while (min < max)
 	{
 		range[i] = min;
 		i++;
@@ -38,7 +38,7 @@ int	*ft_range(int min, int max)
 	int *range;
 	range = ft_range(0,10);
 	int i = 0;
-	while (i < 11 && range)
+	while (i < 10)
 	{
 		printf("%d ", range[i]);
 		i++;
